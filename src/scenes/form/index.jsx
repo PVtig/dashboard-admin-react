1a10987d3a2db6f8f52057103ff58f89526db10d
PVtig <puchko.vlad@gmail.com>
2024-05-19 12:56:09 +0200

    add post data

--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -1,14 +1,23 @@
 import { Box, Button, TextField } from "@mui/material";
 import { Formik } from "formik";
-import * as yup from "yup";
+//import * as yup from "yup";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../components/Header";
+import axios from "axios";
 
 const Form = () => {
   const isNonMobile = useMediaQuery("(min-width:600px)");
 
   const handleFormSubmit = (values) => {
-    console.log(values);
+      const src = 'http://localhost/report'
+      axios.post(src, values)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    console.log(initialValues);
   };
 
   return (
@@ -18,7 +27,6 @@ const Form = () => {
       <Formik
         onSubmit={handleFormSubmit}
         initialValues={initialValues}
-        validationSchema={checkoutSchema}
       >
         {({
           values,
@@ -37,87 +45,75 @@ const Form = () => {
                 "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
               }}
             >
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="car"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.car}
-                name="car"
-                error={!!touched.car && !!errors.car}
-                helperText={touched.car && errors.car}
-                sx={{ gridColumn: "span 2" }}
-              />
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="user"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.user}
-                name="user"
-                error={!!touched.user && !!errors.user}
-                helperText={touched.user && errors.user}
-                sx={{ gridColumn: "span 2" }}
-              />
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="Mileage"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.mileage}
-                name="mileage"
-                error={!!touched.mileage && !!errors.mileage}
-                helperText={touched.mileage && errors.mileage}
-                sx={{ gridColumn: "span 2" }}
-              />
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="type"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.type}
-                name="type"
-                error={!!touched.type && !!errors.type}
-                helperText={touched.type && errors.type}
-                sx={{ gridColumn: "span 2" }}
-              />
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="namber"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.namber}
-                name="namber"
-                error={!!touched.namber && !!errors.namber}
-                helperText={touched.namber && errors.namber}
-                sx={{ gridColumn: "span 2" }}
-              />
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="Address 2"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.address2}
-                name="address2"
-                error={!!touched.address2 && !!errors.address2}
-                helperText={touched.address2 && errors.address2}
-                sx={{ gridColumn: "span 2" }}
-              />
+            <TextField
+              fullWidth
+              variant="filled"
+              type="number"
+              label="type"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.type}
+              name="type"
+              error={!!touched.type && !!errors.type}
+              helperText={touched.type && errors.type}
+              sx={{ gridColumn: "span 2" }}
+            />
+            <TextField
+              fullWidth
+              variant="filled"
+              type="number"
+              label="mileage"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.mileage}
+              name="mileage"
+              error={!!touched.mileage && !!errors.mileage}
+              helperText={touched.mileage && errors.mileage}
+              sx={{ gridColumn: "span 2" }}
+            />
+            <TextField
+              fullWidth
+              variant="filled"
+              type="number"
+              label="garage"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.garage}
+              name="garage"
+              error={!!touched.garage && !!errors.garage}
+              helperText={touched.garage && errors.garage}
+              sx={{ gridColumn: "span 2" }}
+            />
+            <TextField
+              fullWidth
+              variant="filled"
+              type="number"
+              label="car_id"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.car_id}
+              name="car_id"
+              error={!!touched.car_id && !!errors.car_id}
+              helperText={touched.car_id && errors.car_id}
+              sx={{ gridColumn: "span 2" }}
+            />
+            <TextField
+              fullWidth
+              variant="filled"
+              type="number"
+              label="user_id"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.user_id}
+              name="user_id"
+              error={!!touched.user_id && !!errors.user_id}
+              helperText={touched.user_id && errors.user_id}
+              sx={{ gridColumn: "span 2" }}
+            />
+            
             </Box>
             <Box display="flex" justifyContent="end" mt="20px">
-              <Button type="submit" color="secondary" variant="contained">
+              <Button  type="submit" color="secondary" variant="contained">
                 Create New REPORT
               </Button>
             </Box>
@@ -128,27 +124,12 @@ const Form = () => {
   );
 };
 
-const phoneRegExp =
-  /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
-
-const checkoutSchema = yup.object().shape({
-  firstName: yup.string().required("required"),
-  lastName: yup.string().required("required"),
-  email: yup.string().email("invalid email").required("required"),
-  contact: yup
-    .string()
-    .matches(phoneRegExp, "Phone number is not valid")
-    .required("required"),
-  address1: yup.string().required("required"),
-  address2: yup.string().required("required"),
-});
 const initialValues = {
-  firstName: "",
-  lastName: "",
-  email: "",
-  contact: "",
-  address1: "",
-  address2: "",
+  type: "",
+  mileage: "",
+  garage: "",
+  car_id: "",
+  user_id: ""
 };
 
 export default Form;
