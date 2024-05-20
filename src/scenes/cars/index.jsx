3d3e5471db1a41e11302377f6f47edf2359fb5d4
P_WebDew <puchko.vlad@gmail.com>
2024-05-20 13:08:57 +0200

    add forms - car, users

---
+++ src/scenes/cars/index.jsx
@@ -0,0 +1,90 @@
+import { Box, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import Header from "../../components/Header";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const Cars = () => {
+  const src = 'http://localhost/car'
+  const [articles, setArticles] = useState([]);
+  useEffect(()=>{
+  axios
+    .get(src)
+    .then(resalt => {
+      setArticles(resalt.data);
+    })  
+  },[]);
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const columns = [
+    { field: "id", headerName: "ID" },
+    {
+      field: "namber",
+      headerName: "Number",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "type",
+      headerName: "Type",
+      type: "type",
+      headerAlign: "left",
+      align: "left",
+    },
+    {
+      field: "mileage",
+      headerName: "Mileage",
+      flex: 1,
+    },
+    {
+      field: "garage_id",
+      headerName: "Garage",
+      flex: 1,
+    },
+    {
+      field: "status",
+      headerName: "Status",
+      flex: 1,
+    },
+  ];
+
+  return (
+    <Box m="20px">
+      <Header title="TEAM" subtitle="Managing the Team Members" />
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid checkboxSelection rows={articles} columns={columns} />
+      </Box>
+    </Box>
+  );
+};
+
+export default Cars;
